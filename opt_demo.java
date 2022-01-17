a253a0d7ed64badfe16449267d31436314746fde
PritiYad <97586570+PritiYad@users.noreply.github.com>
2022-01-17 11:42:50 +0530

    Add files via upload

---
+++ opt_demo.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class opt_demo
+{
+	public static void main(String args[])
+	{
+		int n1,n2;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two numbers");
+		n1=sc.nextInt();
+		n2=sc.nextInt();
+		
+		n2+=n1;
+		System.out.println("The n2+=n1  \n the value of n2="+ n2);
+	}
+}
+/*
+++num;
+sizeof(num);
+
+Arith
++- * /%
+/=Q
+
+%=R
+ass
+=
+a=10;
++=
+a+=10;
+a=a+10
+a=20
+-=
+a=a-10;
+
+*=
+a=a*10;
+/=
+
+a=a/10
+
+a=a%10
+
+
+
+
+bit
+
+logic
+rela
+*/
